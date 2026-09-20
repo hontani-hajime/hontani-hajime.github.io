ef7f11d87ae68a204bfdb91130fb172eb620f85c
hhps220312 <hontanihajime220312@gmail.com>
2026-09-20 12:35:47 +0900

    Update blog-data.js

--- blog-data.js
+++ blog-data.js
@@ -3,7 +3,6 @@ const blogArticles = [
         title: "神戸文化祭のお知らせ", 
         date: "2025.10.31", 
         tags: [{name: "ボランティア", class: "tag-volunteer"}], 
-        isNew: true, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/2025103101/" 
     },
@@ -11,7 +10,6 @@ const blogArticles = [
         title: "秋の気配を感じて", 
         date: "2026.09.06", 
         tags: [{name: "写真", class: "tag-photo"}], 
-        isNew: false, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/" 
     },
@@ -19,7 +17,6 @@ const blogArticles = [
         title: "春の桜を撮りに行きました", 
         date: "2026.08.31", 
         tags: [{name: "写真", class: "tag-photo"}, {name: "旅行", class: "tag-travel"}], 
-        isNew: false, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/" 
     },
@@ -27,7 +24,6 @@ const blogArticles = [
         title: "新しいサイトのポートフォリオを作成中", 
         date: "2026.08.20", 
         tags: [{name: "WEB・IT", class: "tag-webit"}, {name: "勉強", class: "tag-study"}], 
-        isNew: false, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/" 
     },
@@ -35,7 +31,6 @@ const blogArticles = [
         title: "久しぶりのピアノ発表会に向けて", 
         date: "2026.08.15", 
         tags: [{name: "ピアノ", class: "tag-piano"}], 
-        isNew: false, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/" 
     },
@@ -43,8 +38,15 @@ const blogArticles = [
         title: "日常のスケッチとカフェ巡り", 
         date: "2026.07.10", 
         tags: [{name: "その他", class: "tag-other"}], 
-        isNew: false, 
         img: "https://hontani-hajime.github.io/file/noimage.jpg", 
         url: "https://hontani-hajime.github.io/blog/" 
     }
 ];
+
+// 日付（date）の降順（新しい順）に並び替える
+blogArticles.sort((a, b) => (a.date < b.date ? 1 : -1));
+
+// 最新の1件（配列の先頭）にのみ NEW (isNew = true) をつけ、それ以外は false にする
+blogArticles.forEach((article, index) => {
+    article.isNew = (index === 0);
+});
